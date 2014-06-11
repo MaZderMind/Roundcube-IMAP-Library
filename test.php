b495adfcfc2d5eaf5c3e14bbec7c0f207a9ebbac
MaZderMind <github@mazdermind.de>
2014-06-11 23:55:18 +0200

    remove line-ending whitespace in test-file

--- test.php
+++ test.php
@@ -18,40 +18,40 @@ foreach($imap->list_headers() as $header)
 {
 	// parsed header
 	//print_r($header);
-	
+
 	// parsed systemical msg
 	//$msg = $imap->get_message($header->uid);
-	
+
 	// conains header information, too
 	//var_dump($msg->subject);
 	//print_r($msg->structure);
-	
+
 	// parsed & analyzed logical message
 	$logical = new rcube_message($imap, $header->uid);
-	
+
 	echo "= ".$header->subject;
 	echo "\n=== from ===\n";
 	print_r($logical->sender);
-	
+
 	echo "\n=== to ===\n";
 	print_r($logical->receiver);
-	
+
 	echo "\n=== text ===\n";
 	echo $logical->first_text_part();
-	
+
 	echo "\n=== html ===\n";
 	echo $logical->first_html_part();
-	
+
 	echo "\n=== attachments ===\n";
 	print_r($logical->attachments);
-	
+
 	foreach ($logical->attachments as $idx => $attachment) {
 		echo "\n=== attachment[$idx] ===\n";
 
 		$data = $logical->get_part_content($attachment->mime_id);
 		printf("read %u bytes of %s-data\n", strlen($data), $attachment->mimetype);
 	}
-	
+
 	echo "\n\n\n\n==================================================================================================================================\n\n\n\n\n";
 }
 
